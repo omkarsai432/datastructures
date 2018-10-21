f5f5ee19a1e86904e5cd210098a0e28341c2c3b9
omkar <goli.sai432@gmail.com>
2018-10-21 18:30:09 +0530

    LinkedList

---
+++ LinkedList.py
@@ -0,0 +1,20 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linkedlist:
+    def __init__(self):
+        self.data=None
+    def listprint(self):
+        head=self.data
+        while head is not None:
+            print(head.data)
+            head=head.next
+list=linkedlist()
+list.data=node("Mon")
+e2=node("Tue")
+e3=node("Wed")
+
+list.data.next=e2
+e2.next=e3
+list.listprint()
